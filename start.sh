6d10ae58af16eb81a80281883eff079b25e96c07
lastpicture <humer@csystems.at>
2017-09-18 23:13:42 +0200

    afd

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+mkdir /tmp/session
+mkdir /tmp/wsdlcache
+chown 1002:1002 /tmp/session
+chown 1002:1002 /tmp/wsdlcache
+chmod 600 /tmp/session
+chmod 600 /tmp/wsdlcache
+
 nginx
 php-fpm -F
