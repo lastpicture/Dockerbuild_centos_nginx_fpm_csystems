d77fbdcd3d9bdcddd69b49ace54c43fede4d2d09
lastpicture <humer@csystems.at>
2017-09-19 10:46:52 +0200

    RC8

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ mkdir /tmp/session
 mkdir /tmp/wsdlcache
 chown 1002:1002 /tmp/session
 chown 1002:1002 /tmp/wsdlcache
-chmod 600 /tmp/session
-chmod 600 /tmp/wsdlcache
+chmod 760 /tmp/session
+chmod 760 /tmp/wsdlcache
 
 nginx
 php-fpm -F
